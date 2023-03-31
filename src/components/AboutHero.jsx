49ddbdd609b5e19c4370dbf0ad09edee10dfa177
Emre Altuğ Kırman <110311214+ealtugkirman@users.noreply.github.com>
2023-03-31 18:23:58 +0300

    feedback done.

--- src/components/AboutHero.jsx
+++ src/components/AboutHero.jsx
@@ -3,6 +3,10 @@ import { useInView } from "react-intersection-observer";
 import CountUp from "react-countup";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
+import Lottie from "lottie-react";
+import about1 from "../assets/about1.json";
+import about2 from "../assets/hero3.json";
+import about3 from "../assets/hero-left.json";
 
 function AboutHero() {
   const [ref, inView] = useInView({
@@ -10,20 +14,22 @@ function AboutHero() {
   });
 
   return (
-    <section id="about" className="section" ref={ref}>
-      <div className="bg-yellow">
+    <section id="about" ref={ref}>
+      <div className="bg-yellow mx-24 lg:mx-48">
         <motion.div
           variants={fadeIn("up", 0.5)}
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: false, amount: 0.3 }}
-          className="font-first mt-24 lg:mt-48 text-4xl text-main text-center lg:text-7xl">
+          className="font-first mt-24 lg:mt-64 text-4xl text-main text-center lg:text-7xl">
           <h1>Delivering Exceptional Technology Solutions</h1>
         </motion.div>
-        <motion.div  variants={fadeIn("right", 0.5)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0.3 }} className="font-first text-xl mx-12 lg:mx-24 my-14 text-white text-center">
+        <motion.div
+          variants={fadeIn("right", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="font-first text-xl mx-12 lg:mx-24 my-14 text-white text-center">
           <p>
             Tecvity is a leading software development company located in
             Gujranwala, Pakistan. We are committed to develop innovative
@@ -32,10 +38,34 @@ function AboutHero() {
             Years In Business 50+ Clients Trust Us
           </p>
         </motion.div>
-        <motion.div  variants={fadeIn("down", 0.5)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0.3 }} className="flex-row text-main mx-auto justify-center flex">
+        <motion.div
+          variants={fadeIn("right", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="w-1/6 lg:w-2/12 absolute top-32 left-10 lg:top-48">
+          <Lottie animationData={about1} loop={true} />
+        </motion.div>
+        <motion.div
+          variants={fadeIn("left", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="w-1/5 lg:w-1/6 absolute bottom-24 right-10 lg:bottom-48">
+          <Lottie animationData={about2} loop={true} />
+        </motion.div>
+        <motion.div variants={fadeIn("right", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }} className="w-1/4 lg:w-1/5 absolute bottom-14 left-10 lg:left-24 lg:bottom-8">
+          <Lottie animationData={about3} loop={true} />
+        </motion.div>
+        <motion.div
+          variants={fadeIn("down", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="flex-row text-main mx-auto pb-32 justify-center flex">
           <div>
             <div className="text-[40px] mx-2 lg:mx-8 font-first mb-2">
               {inView ? <CountUp start={0} end={4} duration={3} /> : null}
