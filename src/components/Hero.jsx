193d3a743a362f84953a27d3f9ab982df14c62fe
Najam Ul Saqib <najamulsaqib@tutamail.com>
2023-04-06 23:07:30 +0500

    update the tagline

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ function Hero() {
           viewport={{ once: false, amount: 0.3 }}
           className="absolute top-40 lg:top-64 left-0 right-0 bottom-0 font-first pb-4 lg:pb-24 text-white text-center items-center flex flex-col z-10">
           <h1 className="bg-gradient-to-r from-main bg-clip-text via-blue-200 to-green-800 inline-block text-transparent font-bold py-4 lg:pb-14 text-5xl hover:text-main lg:text-7xl">
-            Creative Technological Solutions
+            Delivering Creative Technological Solutions
           </h1>
           <p className="text-2xl py-4 text-white hover:text-main">
             We see what others don't
